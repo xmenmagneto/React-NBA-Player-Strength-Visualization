a81524244f0121b69d01fd47a46fe7739dc87ac3
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-20 00:05:15 -0600

    finish stage1

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,25 +5,27 @@ import {Profile} from "./Profile"
 
 export class Main extends React.Component {
     state = {
-        playerId: nba.findPlayer('Stephen Curry').playerId,  //render ShotChart
-        playerInfo: {}, //render Profile
+        playerId: nba.findPlayer('Stephen Curry').playerId,
+        playerInfo: {},
     }
 
-    ComponentDidMount() {
+    componentDidMount() {
         nba.stats.playerInfo({ PlayerID: this.state.playerId }).then((info) => {
-            const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]); //merge 两个object
+            const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
             console.log(playerInfo);
             this.setState({
                 playerInfo: playerInfo,
             });
         });
     }
+
     render() {
         return (
             <div className="main">
-                <Profile/>
+                <Profile playerId={this.state.playerId} playerInfo={this.state.playerInfo}/>
                 <ShotChart playerId={this.state.playerId}/>
             </div>
         );
     }
+
 }
