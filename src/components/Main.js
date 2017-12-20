75a7bc22e322f4196de8be63b6d0a5d4d5ab64bf
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-19 22:47:59 -0600

    add ShotChart.css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {ShotChart} from "./ShortChar"
+import {ShotChart} from "./ShortChar";
+import nba from 'nba';
 
 export class Main extends React.Component {
+    state = {
+        playerId: nba.findPlayer('Stephen Curry').playerId
+    }
+    ComponentDidMount() {
+
+    }
     render() {
         return (
             <div className="main">
-                <ShotChart playerId={}/>
+                <ShotChart playerId={this.state.playerId}/>
             </div>
         );
     }
