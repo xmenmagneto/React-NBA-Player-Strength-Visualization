3cf1464a21ec9329a9ddfb4e15bb5b6417b2219f
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-19 22:34:13 -0600

    add ShortChar.js

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ShotChart} from "./ShortChar"
+
+export class Main extends React.Component {
+    render() {
+        return (
+            <div className="main">
+                <ShotChart playerId={}/>
+            </div>
+        );
+    }
+}
