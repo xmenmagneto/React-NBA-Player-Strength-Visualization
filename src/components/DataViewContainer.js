971a5e7896154d99a9ca9c75ea2da574339c6fb7
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-22 21:52:11 -0600

    add slider UI

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {ShotChart} from "./ShortChar"
+import {ShotChart} from "./ShortChar";
+import {CountSlider} from "./CountSlider"
+
 
 export class DataViewContainer extends React.Component {
     render() {
         return(
             <div className="data-view">
                 <ShotChart playerId={this.props.playerId}/>
-                <div classname="filters">
-
+                <div className="filters">
+                    <CountSlider/>
                 </div>
             </div>
         );
